f6e672e89725f9f99f7ba9a67ba8aa2f688b3ea9
kyounghwan <osc9245@naver.com>
2019-04-04 14:21:13 +0900

    do while, while, for 문 example

--- lecture03.js
+++ lecture03.js
@@ -38,42 +38,42 @@ for(count;count<3;count++){
 }
 document.write("bye");
 
-// var cost = [ 85, 42, 37, 10, 22, 8, 15 ];
-// var total_cost = 0;
-// // 여기에 코드를 작성하세요.
-// for(var i=0; i<cost.length; i++){
-//     total_cost=total_cost+cost[i];
-// }
-// console.log(total_cost);
+var cost = [ 85, 42, 37, 10, 22, 8, 15 ];
+var total_cost = 0;
+// 여기에 코드를 작성하세요.
+for(var i=0; i<cost.length; i++){
+    total_cost=total_cost+cost[i];
+}
+console.log(total_cost);
 
 
 
-// for(var i=0;i<11;i++){
-//     if(i%2==0){
-//         continue;
-//     }
-//     document.write(i+"<br>");
-//     continue;
-// }
+for(var i=0;i<11;i++){
+    if(i%2==0){
+        continue;
+    }
+    document.write(i+"<br>");
+    continue;
+}
 
-// var arr=[1,2,3,4,5,6,7,8,9,10];
-// var i=0;
-// var j=0;
+var arr=[1,2,3,4,5,6,7,8,9,10];
+var i=0;
+var j=0;
 
-// while(i <arr.length){
-//     document.write(arr[i]+"<br>");
+while(i <arr.length){
+    document.write(arr[i]+"<br>");
 
-//     i++
-// }
+    i++
+}
 
-// for(i=2;i<10;i++){
-//     for(var j=1;j<10;j++){
-//     document.write(i*j);
-//     }
-//     document.write("<br>");
-// }
+for(i=2;i<10;i++){
+    for(var j=1;j<10;j++){
+    document.write(i*j);
+    }
+    document.write("<br>");
+}
 
-// do{
-//     var ans= parseInt(prompt("1+1=?"))
-// }while(ans!=2)
-//     document.write("정답이 아닙니다. 다시 푸세요");
+do{
+    var ans= parseInt(prompt("1+1=?"))
+}while(ans!=2)
+    document.write("정답이 아닙니다. 다시 푸세요");
